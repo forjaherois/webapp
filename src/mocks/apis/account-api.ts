7652f7cc5ed8e46b19821de23b8c3a6bf769fe07
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-05 13:39:55 -0300

    refactor: remove msw

--- src/mocks/apis/account-api.ts
+++
@@ -1,9 +0,0 @@
-import { RestRequest, ResponseComposition, RestContext } from 'msw';
-
-export const postSignup = (
-    req: RestRequest,
-    res: ResponseComposition,
-    ctx: RestContext
-) => {
-    return res(ctx.delay(3000), ctx.status(204));
-};
--- src/mocks/handlers.ts
+++
@@ -1,4 +0,0 @@
-import { rest } from 'msw';
-import { postSignup } from './apis/account-api';
-
-export const handlers = [rest.post('/account', postSignup)];
