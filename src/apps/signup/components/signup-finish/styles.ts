f92beecbb84f8deeada5c9ae377179988308a817
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-06 21:14:56 -0300

    feat: add signup finish

---
+++ src/apps/signup/components/signup-finish/styles.ts
@@ -0,0 +1,11 @@
+import { Grid } from '@nextui-org/react';
+import { styled } from 'styled-components';
+
+export const WrapperFinish = styled(Grid)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    height: 100vh;
+`;
--- src/apps/signup/signup-page.tsx
+++ src/apps/signup/signup-page.tsx
@@ -1,12 +1,14 @@
 import { Grid } from '@nextui-org/react';
 import { SignupLogo } from './components/signup-logo/signup-logo';
 import { SignupForm } from './components/signup-form/signup-form';
+import { SignupFinish } from './components/signup-finish/signup-finish';
 
 export const SignupPage = () => {
+    const signFinish = true;
     return (
         <Grid.Container>
             <SignupLogo />
-            <SignupForm />
+            {signFinish ? <SignupFinish /> : <SignupForm />}
         </Grid.Container>
     );
 };
