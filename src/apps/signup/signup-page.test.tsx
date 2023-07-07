166fbbe80f48db7022475bff3b57dfd767bd61f5
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-07 00:28:21 -0300

    test: add test in signup page

--- src/apps/signup/signup-page.test.tsx
+++ src/apps/signup/signup-page.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { SignupContext } from '@src/contexts/signup-context';
 import { SignupPage } from './signup-page';
 
 jest.mock('./components/signup-logo/signup-logo', () => {
@@ -15,11 +16,45 @@ jest.mock('./components/signup-form/signup-form', () => {
     };
 });
 
+jest.mock('./components/signup-finish/signup-finish', () => {
+    return {
+        __esModule: true,
+        SignupFinish: () => <div data-testid='signupFinish' />,
+    };
+});
+
 describe('SignupPage Component', () => {
-    it('renders SignupLogo and SignupForm', () => {
-        render(<SignupPage />);
+    it('renders SignupLogo and SignupForm when signupFinish is false', () => {
+        const contextValue = {
+            signupFinish: false,
+            handleSignupFinsih: jest.fn(), // mock function
+        };
+
+        render(
+            <SignupContext.Provider value={contextValue}>
+                <SignupPage />
+            </SignupContext.Provider>
+        );
 
         expect(screen.getByTestId('signupLogo')).toBeInTheDocument();
         expect(screen.getByTestId('signupForm')).toBeInTheDocument();
+        expect(screen.queryByTestId('signupFinish')).not.toBeInTheDocument();
+    });
+
+    it('renders SignupLogo and SignupFinish when signupFinish is true', () => {
+        const contextValue = {
+            signupFinish: true,
+            handleSignupFinsih: jest.fn(), // mock function
+        };
+
+        render(
+            <SignupContext.Provider value={contextValue}>
+                <SignupPage />
+            </SignupContext.Provider>
+        );
+
+        expect(screen.getByTestId('signupLogo')).toBeInTheDocument();
+        expect(screen.getByTestId('signupFinish')).toBeInTheDocument();
+        expect(screen.queryByTestId('signupForm')).not.toBeInTheDocument();
     });
 });
