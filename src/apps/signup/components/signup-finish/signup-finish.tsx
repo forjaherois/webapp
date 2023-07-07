f1366d5c2be47d3f56ef73ad387d76867ff538af
Daniel Barreto <munizbarreto5@gmail.com>
2023-07-06 21:25:37 -0300

    refactor: ajustment check image margin

--- src/apps/signup/components/signup-finish/signup-finish.tsx
+++ src/apps/signup/components/signup-finish/signup-finish.tsx
@@ -7,7 +7,7 @@ export const SignupFinish = () => {
             <Grid>
                 <img style={{ display: 'flex' }} width={90} height={90} src='https://i.imgur.com/wjIW3sW.png' />
             </Grid>
-            <Grid css={{ maxWidth: '400px', marginTop: '40px' }}>
+            <Grid css={{ maxWidth: '400px', marginTop: '35px' }}>
                 <Text size={'$lg'} weight={'semibold'} css={{ textAlign: 'center', lineHeight: '$sm' }}>
                     Sua conta foi criada com sucesso!
                     <br /> Enviamos um link de confirmação para o seu e-mail.
